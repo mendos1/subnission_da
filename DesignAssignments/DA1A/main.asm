728922176bfc1389d4887ba9b2f70c8dfe6ae0df
Saul Mendoza Guzman <mendos1@ad.unlv.edu>
2019-02-10 19:11:44 -0800

    Deleted Old/Wrong Files and replaced with correct

--- DesignAssignments/DA1A/main.asm
+++ DesignAssignments/DA1A/main.asm
@@ -4,48 +4,49 @@
 ;	Student Email: mendos1@unlv.nevada.edu
 ;
 
-.org	0x0000
+.org 0x0000 
+	
+	ldi		r16, 0x08		; Decrementing counter control
+	clr		r18			; Product Low Bytes
+	clr		r19			; Product Mid Bytes
+	clr		r20			; Product High Bytes
+	ldi		r21, 0x00		; Always Zero
+	ldi		r22, 0xFF		; Plier
+	ldi		r23, 0x00		; Virtual High Bytes for Cand
+	ldi		r24, 0xFF		; Cand Low Bytes
+	ldi		r25, 0xFF		; Cand Mid Bytes
 
-	ldi		r24, 0x11		; This is the 
-	ldi		r25, 0x11		; Multiplicand
-
-	ldi		r22, 0x11		; This is the multiplier 
-
-	ldi		r16, 0x08		; This is the # of shifts 
-
-	clr		r18				; This is where the
-	clr		r19				; Result of the
-	clr		r20				; Program will be stored
-
-	clr		r21				; This is always Zero
-
-ShiftCheck:
-	cpi		r16, 0x00		; Compare reg 16 to 0
-	brne	CheckLP			; If reg 16 is not equal to 0 we branch
-	jmp	Done				; If they are equal we are done
-
-CheckLP:
-	mov		r17, r22		; Temp reg with value of r22
-	andi	r17, 0b00000001	; Clear all bits besides the first one
-							; To check for 1 or 0 in bit one
-
-	cpi		r17, 0b00000001	; Compare first bits
-	breq	AddCand			; If equal: branch to label "AddCand"
-	brne	ShiftStage		; If !equal: branch to label "ShiftStage"
-
-AddCand:
-	add		r18, r24		; Add low bits
-	adc		r19, r25		; Add mid bits with carry bit
-	adc		r20, r21		; Add high bits with carry bit
+BitCheck:
+	
+	mov		r17, r22		; Bit Checking Register 
+	andi	r17, 0b00000001			; Logical AND Bit Check Set Up
+	brne	AddCandStage		; 1 Is Detected As First Bit
+	breq	ShiftStage		; 0 Is Detected As First Bit
 
 ShiftStage:
-	lsl		r25				; Logic Shift Left of reg 25
-	lsl		r24				; Logic Shift Left of reg 24
-	adc		r25, r21		; Add carry bit to reg 25. r21 is Zero
-	lsr		r22				; Logic Shift Right of reg 22
-	dec		r16				; Decriment r16 
-	rjmp	ShiftCheck		; Relative Jump to "ShiftCheck" 
 
-Done:						; Done
-	BREAK
- 
+	lsl		r23			; Logical Shift Left Of Virtual Bytes
+	lsl		r25			; Logical Shift Left Of High Bytes of Cand
+	adc		r23, r21		; Add Carry Bit Of Previouse Step To Virtual Bytes Of Cand
+	lsl		r24			; Logical Shift Left Of Low Bytes Of Cand
+	adc		r25, r21		; Add Carry Bit of Previouse Step To High Bytes
+	
+	lsr		r22			; Logical Shift Right Of Plier
+	dec		r16			; Decrement Bit/Shift Counter
+	brne	BitCheck		; If Not Zero Branch To Label BitCheck
+	breq	Done		; If Zero Branch To Label Done
+
+AddCandStage:
+
+	add		r18, r24		; Add Low Bytes Of Product And Cand respectively
+	adc		r19, r21		; Add Carry Bit To Mid Reg Of Product
+	adc		r20, r21		; Add Propagated Bit To High Bytes of Product
+	add		r19, r25		; Add Mid/High Bytes of Product And Cand Respectively
+	adc		r20, r21		; Add Carry Bit to Higher 
+	add		r20, r23		; Add High Byte of Product with Virtual Byte of Cand
+
+	rjmp	ShiftStage
+
+Done:
+	
+	BREAK 
