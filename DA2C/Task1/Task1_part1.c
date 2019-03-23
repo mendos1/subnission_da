fce709326f9d758acfa540ca51a7a3c49294ac06
mendos1 <47402221+mendos1@users.noreply.github.com>
2019-03-22 19:28:03 -0700

    Add files via upload

---
+++ DA2C/Task1/Task1_part1.c
@@ -0,0 +1,31 @@
+#define F_CPU 16000000UL
+#include <avr/io.h>
+// THIS IS TASK1 PART1
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+int main(void){
+	DDRB = 36;
+	PORTB |= (1<<5);
+	TCCR0B = 5;
+	while(1){
+		TCNT0 = 0;
+		int TACO = 0;
+		while(TACO != 27){
+			while(TCNT0 != 255);
+			TCNT0 = 0;
+			TACO++;
+		}
+		PORTB ^= (1<<2);
+		TACO = 0;
+		TCNT0 = 0;
+		while(TACO != 18){
+			while(TCNT0 != 255);
+			TCNT0 = 0;
+			TACO++;
+		}
+		PORTB ^= (1<<2);
+		TACO = 0;
+		TCNT0 = 0;
+	}
+}
